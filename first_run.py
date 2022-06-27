e550760ea2d7627ed20498243dfd21daa872afd8
mohsiniscoding <mohsin.doer@gmail.com>
2022-06-28 03:36:50 +0500

    fixed first_run

--- first_run.py
+++ first_run.py
@@ -1,5 +1,5 @@
 ## run this file first if you are setting up flask for first time
 from ruth_tts_transformer.api import TTS
 tts = TTS()
-tts.generate(text)
+tts.generate("You guys are bunch of care takers!")
 file_name = tts.parse() + ".wav"
